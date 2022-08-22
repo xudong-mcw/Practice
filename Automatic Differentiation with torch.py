8b4911bda28cffa6101ded255eaaa038bd68e688
18504403602 <18504403602@163.com>
2022-08-22 09:24:34 +0800

    add code

---
+++ Automatic Differentiation with torch.py
@@ -0,0 +1,7 @@
+import torch
+x = torch.ones(5)
+y = torch.zeros(3)
+w = torch.randn(5,3,requires_grad=True)
+b = torch.randn(3,requires_grad=True)
+z = torch.matmul(x,w)+b
+loss = torch.nn.functional.binary_cross_entropy_with_logits(z,y)
